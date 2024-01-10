652cfb0f83175cd6dd106fa2f575e684ee1ee15d
AlanEA04 <omar.encinas.cetis69@gmail.com>
2024-01-10 09:12:43 -0700

    Terminar Logica

--- AppCriticas/AppCriticas/MainPage.xaml.cs
+++ AppCriticas/AppCriticas/MainPage.xaml.cs
@@ -10,9 +10,31 @@ namespace AppCriticas
 {
     public partial class MainPage : ContentPage
     {
+        
         public MainPage()
         {
             InitializeComponent();
+        }
+
+        private void MostrarCriticas_Clicked(object sender, EventArgs e)
+        {
+            string nombre = Nombre.Text;
+            string genero = Hombre.IsChecked ? "Hombre" : "Mujer";
+            string caracteristicas = "";
+
+            if (Alto.IsChecked)
+                caracteristicas += "ALTO ";
+            if (Feo.IsChecked)
+                caracteristicas += "FEO ";
+            if (Listo.IsChecked)
+                caracteristicas += "LISTO y ";    
+            if (Extravagante.IsChecked)
+                caracteristicas += "EXTRAVAGANTE. ";
+
+            LabelCritica.Text = $"{nombre} es {genero} tambien es {caracteristicas.TrimEnd(' ', ',')}";
+         
+
+            DatosIngresados.IsVisible = true;
         }
     }
 }
